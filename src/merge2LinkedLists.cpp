b92ab3326185f42372e1d0007c1dc9213dd67abc
hemuinakollu <hemuinakollu@missionrnd.com>
2016-03-29 07:48:25 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,90 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include <malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	if (head1==NULL)
+			return head2;
+	if (head2 == NULL)
+			return head1;
+	
+		struct node *head = (struct node*)malloc(sizeof(struct node));
+		head = NULL;
+		struct node *i = head;
+		while (head1 != NULL&&head2 != NULL)
+		{
+			if (head1->num <= head2->num)
+			{
+				struct node *n = (struct node*)malloc(sizeof(struct node));
+				n->num = head1->num;
+				n->next = NULL;
+				head1= head1->next;
+				if (head == NULL){
+					head = n;
+					i = n;
+				}
+				else{
+					i->next = n;
+					i = n;
+				}
+				
+			}
+			else if (head1->num >= head2->num)
+			{
+				struct node *n = (struct node*)malloc(sizeof(struct node));
+				n->num = head2->num;
+				n->next = NULL;
+				head2 = head2->next;
+				if (head == NULL){
+					head = n;
+					i = n;
+				}
+
+				else{
+					i->next = n;
+					i = n;
+				}
+			}
+			
+
+		}
+		while (head1 != NULL)
+		{
+			struct node *n = (struct node*)malloc(sizeof(struct node));
+			n->num = head1->num;
+			n->next = NULL;
+			head1 = head1->next;
+			if (head == NULL)
+			{
+				head = n;
+				i = n;
+			}
+			else{
+				i->next = n;
+				i = n;
+			}
+		}
+		while (head2!=NULL)
+		{
+			struct node *n = (struct node*)malloc(sizeof(struct node));
+			n->num = head2->num;
+			n->next = NULL;
+			head2 = head2->next;
+			if (head == NULL)
+			{
+				head = n;
+				i = n;
+			}
+			else{
+				i->next = n;
+				i = n;
+			}
+		}
+		return head;
+	
 }
